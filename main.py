5da7a02f9c077d5a7b914d22c7e6dfbeb098dab1
Vadik <vadikschool248@gmail.com>
2022-02-10 19:12:33 +0300

    Add button and label in main.py

--- main.py
+++ main.py
@@ -1,7 +1,24 @@
 from kivy.app import App
+from kivy.uix.button import Button
+from kivy.uix.label import Label
+from kivy.uix.boxlayout import BoxLayout
+
 
 class MainApp(App):
-    pass
+
+    def build(self):
+        box = BoxLayout(orientation='vertical')
+        btn = Button(text='Моя первая кнопка!', on_press=self.on)
+        global label
+        label = Label()
+        box.add_widget(btn)
+        box.add_widget(label)
+        return box
+
+    def on(self, *args):
+        label.text = 'Hello, kivy!'
+
+
 
 if __name__ == '__main__':
     app = MainApp()
